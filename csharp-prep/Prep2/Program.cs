2fe04c5bcf6e75b676ea3230c0e569276606968e
80am <aclaycamp@gmail.com>
2024-05-01 15:34:41 -0700

    Grading system

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string lgrade = "";
+
+        if (percent >= 90)
+        {
+            lgrade = "A";
+
+        }
+        else if (percent >= 80)
+        {
+            lgrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            lgrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            lgrade = "D";
+        }
+        else 
+        {
+            lgrade = "F";
+        }
+        Console.WriteLine($"Your grade is: {lgrade}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You Passed the Grade!");
+        }
+        else
+        {
+            Console.WriteLine("You can always try again next time");
+        }
+
     }
 }
